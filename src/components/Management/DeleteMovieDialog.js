9a84b40175359274d5938b1402f694211388e84e
alperylmztrk <alperylr@gmail.com>
2023-06-22 17:53:55 +0300

    admin paneli iyileştirmeleri

---
+++ src/components/Management/DeleteMovieDialog.js
@@ -0,0 +1,56 @@
+import { Autocomplete, Button, Stack, TextField, Dialog, Typography, Snackbar, Alert, AlertTitle, Box, IconButton } from "@mui/material";
+import * as React from 'react';
+import { LocalizationProvider, DateTimePicker } from "@mui/x-date-pickers";
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { format } from 'date-fns';
+import { useState, useEffect } from "react";
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function DeleteMovieDialog(props) {
+
+
+
+
+
+
+    const handleSil = () => {
+
+        // fetch("/movies" + props.movieId, {
+        //     method: "DELETE",
+        //     headers: { 'Content-Type': 'application/json' },
+        // })
+        return(<div>kapannn</div>);
+        
+    }
+
+
+    return (
+        <div>
+            <Dialog
+                open={props.open}
+                onClose={props.kapat}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Filmi silmek istediğinizden emin misiniz?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {props.movieName} silinecek!! {props.movieId}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={props.kapat}>Vazgeç</Button>
+                    <Button onClick={handleSil} autoFocus>
+                        Sil
+                    </Button>
+                </DialogActions>
+            </Dialog>
+           
+        </div>
+    );
+}
