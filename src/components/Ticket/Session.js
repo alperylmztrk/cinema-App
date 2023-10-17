d9f7ce8950576f00042a03865e36937a1b71a346
alperylmztrk <alperylr@gmail.com>
2023-10-17 17:23:09 +0300

    fix

--- src/components/Ticket/Session.js
+++ src/components/Ticket/Session.js
@@ -35,31 +35,12 @@ TabPanel.propTypes = {
 function Session() {
     const { movieId } = useParams();
     const [value, setValue] = useState(0);
-    const [movie, setMovie] = useState();
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
     const [assignedMovieList, setAssignedMovieList] = useState([]);
     const [errorAssignedMovieList, setErrorAssignedMovieList] = useState(null);
     const [isLoadedAssignedMovieList, setIsLoadedAssignedMovieList] = useState(false);
 
     const monthNames = ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
         "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"];
-
-
-
-    useEffect(() => {
-        fetch("/movies/" + movieId)
-            .then(res => res.json())
-            .then((result) => {
-                setIsLoaded(true);
-                console.log(result)
-                setMovie(result);
-            }, (error) => {
-                setIsLoaded(true);
-                setError(error);
-            }
-            )
-    }, [])
 
     useEffect(() => {
         fetch("/assignedMovies?movieId=" + movieId)
@@ -83,9 +64,9 @@ function Session() {
     };
 
 
-    if (error || errorAssignedMovieList) {
+    if (errorAssignedMovieList) {
         return <div>Error !!!</div>
-    } else if (!isLoaded || !isLoadedAssignedMovieList) {
+    } else if (!isLoadedAssignedMovieList) {
         return (
 
             <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: "center", height: "calc(100vh - 64px)" }}>
@@ -109,11 +90,11 @@ function Session() {
                     display="flex"
                     justifyContent="center"
                     mt={2}>
-                    {movie.title}
+                    {assignedMovieList[0].movieTitle}
                 </Typography>
 
                 <Box display={"flex"} justifyContent={"center"} mt={5}
-                    sx={{ height: 200, bgcolor:"antiquewhite" }}
+                    sx={{ height: 200, bgcolor: "#b7def7" }}
                 >
                     <Tabs
                         orientation="vertical"
@@ -121,28 +102,28 @@ function Session() {
                         value={value}
                         onChange={handleChange}
                         aria-label="Vertical tabs example"
-                        sx={{ borderRight: 1, borderColor: 'divider' }}
+                        sx={{ borderRight: 2, borderColor: 'divider' }}
                     >
                         {assignedMovieList.map((assignedMovie, index) => {
+                            
                             const dateTime = new Date(assignedMovie.startDateTime);
                             const date = dateTime.getDate() + " " + monthNames[dateTime.getMonth()]
-                            console.log(assignedMovie.startDateTime);
-                            console.log(dateTime);
+        
                             return (<Tab label={date} />)
                         })}
                     </Tabs>
 
 
-                    {assignedMovieList.map((assignedMovie, index) => {
+                    {assignedMovieList.map((assignedMovie, index, array) => {
                         const dateTime = new Date(assignedMovie.startDateTime);
                         const time = (dateTime.getHours() < 10 ? "0" : "") + dateTime.getHours() + ":" + (dateTime.getMinutes() < 10 ? "0" : "") + dateTime.getMinutes();
-                        console.log(time);
                         return (<TabPanel value={value} index={index}>
                             <Stack direction="row" spacing={1}>
-                                {assignedMovie.hall.capacity !== assignedMovie.reservedSeats.length ?
+                                {assignedMovie.hallCapacity !== assignedMovie.reservedSeatNum ?
                                     <Chip label={time} variant="outlined" onClick={handleClick} component="a" href={"/movies/sessions/" + assignedMovie.id + "/seats"} clickable />
-                                    : <Chip label={time+" (Dolu)"} variant="outlined"  disabled />
+                                    : <Chip label={time + " (Dolu)"} variant="outlined" disabled />
                                 }
+                            
 
                             </Stack>
                         </TabPanel>)
